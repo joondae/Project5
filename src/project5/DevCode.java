9a36de1fa80ae479be52397e445b9b0be4ee2f01
joondae <68300257+joondae@users.noreply.github.com>
2021-12-10 22:01:25 -0500

    Create DevCode.java
    Created separate file for code and comments used during development so that I can use it as a reference if I decide to refactor things

---
+++ src/project5/DevCode.java
@@ -0,0 +1,21 @@
+package project5;
+
+public class DevCode {
+
+	//MOUNTAINHIKE
+	//validateInputFile()
+	//CONSOLIDATE WITH ABOVE IF STATEMENT?
+	/*
+	if(args[0] == null) {
+		System.err.println("Usage Error:"
+				+ " the program expects file name as an argument.\n");
+		System.exit(1);
+	}
+	*/
+	
+	//processInputFile()
+	//Is there a need to deal with non-visible ascii characters?
+	//\n considered a whitespace delimiter regex used
+	//Split on leading and trailing whitespace (necessary?)
+	//String[] arr = br.readLine().split("\\s");
+}
